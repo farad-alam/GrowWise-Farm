e5c315a84d726f217d16a7b2e05ec9355dd7b85f
Farad Alam <farad.dev@gmail.com>
2025-05-15 23:40:28 +0600

    Update sitemap using nextsitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,13 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "https://grow-wise-farm.netlify.app",
+  generateRobotsTxt: true,
+  transform: async (config, path) => {
+    return {
+      loc: path, // generated url
+      lastmod: new Date().toISOString(),
+      changefreq: "weekly",
+      priority: 0.8,
+    };
+  },
+};
